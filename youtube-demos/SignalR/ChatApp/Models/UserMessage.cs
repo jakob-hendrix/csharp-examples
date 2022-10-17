3c01dcc230eef895c68cba1978d395d39865145f
Jakob Hendrix <jhendrix001@gmail.com>
2022-10-17 17:10:35 -0500

    Basic blazor server app that shows chat hub

---
+++ youtube-demos/SignalR/ChatApp/Models/UserMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Models
+{
+    public class UserMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool IsCurrentUser { get; set; }
+        public DateTime DateSent { get; set; }
+
+    }
+}
