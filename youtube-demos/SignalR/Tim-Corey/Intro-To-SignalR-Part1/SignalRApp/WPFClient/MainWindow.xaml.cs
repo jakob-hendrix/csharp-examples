12c939d584505536332699cc7956a4aeb774e496
Jakob Hendrix <jhendrix001@gmail.com>
2022-12-12 16:57:08 -0600

    Began adding WPF client
    up to 51:22 in video

--- youtube-demos/SignalR/Tim-Corey/Intro-To-SignalR-Part1/SignalRApp/WPFClient/MainWindow.xaml.cs
+++ youtube-demos/SignalR/Tim-Corey/Intro-To-SignalR-Part1/SignalRApp/WPFClient/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.AspNetCore.SignalR.Client;
 
-namespace WPFClient
+namespace WPFClient;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    HubConnection connection;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7260/chat")
+            .WithAutomaticReconnect()
+            .Build();
+
     }
 }
