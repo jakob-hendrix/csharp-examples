7502c5946bcea6a2e8b29872e27fe8714f8e4dd3
Jakob Hendrix <jhendrix001@gmail.com>
2022-12-12 16:43:08 -0600

    Added base chat hub

--- youtube-demos/SignalR/Tim-Corey/Intro-To-SignalR-Part1/SignalRApp/BlazorServer/Program.cs
+++ youtube-demos/SignalR/Tim-Corey/Intro-To-SignalR-Part1/SignalRApp/BlazorServer/Program.cs
@@ -1,6 +1,7 @@
 using BlazorServer.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServer.Hubs;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddResponseCompression(o =>
+{
+    o.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" });
+});
 
 var app = builder.Build();
 
@@ -26,6 +32,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.MapBlazorHub();
+app.MapHub<ChatHub>("/chat");
 app.MapFallbackToPage("/_Host");
 
 app.Run();
