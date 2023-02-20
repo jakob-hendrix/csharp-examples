3914ab3f4dc4c408b6f5ffc28086d7a62f9fe4f0
Jakob Hendrix <jhendrix001@gmail.com>
2023-02-20 16:14:01 -0600

    Added model and EditForm
    Will now map the profile image to a model

---
+++ youtube-demos/Tim-Corey-Blazor-File-Mini-Course/UploadFilesSln/UploadFilesApp/Data/CustomerModel.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UploadFilesApp.Data;
+
+public class CustomerModel
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = "jakob";
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FileName { get; set; }
+}
