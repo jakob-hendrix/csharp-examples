dc1a01785070ff6f18849be1f71ec3628adfaf75
Jakob Hendrix <jhendrix001@gmail.com>
2023-03-21 10:43:56 -0500

    Added blazor ui and base component

---
+++ website-demos/CodeMvvmTutorial/CodeMvvm.Entities/Condition.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeMvvm.Entities;
+
+public class Condition
+{
+    public int? Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [MaxLength(10000)]
+    public string Description { get; set; }
+    [DataType(DataType.Url)]
+    public string URL { get; set; }
+
+    [Range(1,9, ErrorMessage = "Spell Level must be between 1 and 9")]
+    public int SpellLevel { get; set; }
+}
---
+++ website-demos/CodeMvvmTutorial/CodeMvvm.UI/Program.cs
@@ -0,0 +1,33 @@
+using CodeMvvm.UI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+#region Services
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+#endregion
+
+var app = builder.Build();
+
+#region Pipeline
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+#endregion
+
+app.Run();
